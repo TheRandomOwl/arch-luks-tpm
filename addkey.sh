f3a74b989e45a2c7bd16fa55570a6d280e145b82
TheRandomOwl <john.sworsebador@gmail.com>
2021-06-30 19:28:42 -0700

    Updated installer

--- addkey.sh
+++ addkey.sh
@@ -2,7 +2,8 @@
 
 KEY_DIR="/tmp"
 PCR="0,2,4,7"
-tpm2_evictcontrol -C o -c 0x81000000
+ADDRESS="0x81000000"
+tpm2_evictcontrol -C o -c $ADDRESS
 tpm2_createpolicy --policy-pcr -l sha1:$PCR -L policy.digest && \
 tpm2_createprimary -C e -g sha1 -G rsa -c primary.context && \
 dd bs=128 count=1 if=/dev/random of=$KEY_DIR/secret.bin iflag=fullblock && \
@@ -10,4 +11,4 @@ tpm2_create -g sha256 -u obj.pub -r obj.priv -C primary.context -L policy.digest
 tpm2_load -C primary.context -u obj.pub -r obj.priv -c load.context && \
 tpm2_evictcontrol -C o -c load.context 0x81000000 && \
 rm load.context obj.priv obj.pub policy.digest primary.context && \
-echo "Done" ; exit 0 || echo "Something went wrong" ; exit 1
+echo "Done" && exit 0 || echo "Something went wrong" ; exit 1
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-install -Dm644 ./hooks/encrypt-tpm /etc/initcpio/hooks/
-install -Dm644 ./install/encrypt-tpm /etc/initcpio/install
+install -Dm644 -v -C ./hooks/encrypt-tpm /etc/initcpio/hooks && \
+install -Dm644 -v -C ./install/encrypt-tpm /etc/initcpio/install && \
+echo "Done" && exit 0 || exit 1
