658d18281ed38a8833c6b4838fc836a633366d18
TheRandomOwl <46622921+TheRandomOwl@users.noreply.github.com>
2021-06-30 23:07:17 -0700

    Update addkey.sh

--- addkey.sh
+++ addkey.sh
@@ -9,6 +9,6 @@ tpm2_createprimary -C e -g sha1 -G rsa -c primary.context && \
 dd bs=128 count=1 if=/dev/random of=$KEY_DIR/secret.bin iflag=fullblock && \
 tpm2_create -g sha256 -u obj.pub -r obj.priv -C primary.context -L policy.digest -a "noda|adminwithpolicy|fixedparent|fixedtpm" -i $KEY_DIR/secret.bin && \
 tpm2_load -C primary.context -u obj.pub -r obj.priv -c load.context && \
-tpm2_evictcontrol -C o -c load.context 0x81000000 && \
+tpm2_evictcontrol -C o -c load.context $ADDRESS && \
 rm load.context obj.priv obj.pub policy.digest primary.context && \
 echo "Done" && exit 0 || echo "Something went wrong" ; exit 1
